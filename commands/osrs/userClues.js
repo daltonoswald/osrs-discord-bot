7b88b62862c5d0150603c29767bd4f19c108375d
daltonoswald <daltonoswald@gmail.com>
2025-04-18 13:41:26 -0400

    added two new functions for hi scores

--- commands/osrs/userClues.js
+++ commands/osrs/userClues.js
@@ -4,8 +4,8 @@ const { Hiscores } = require('oldschooljs');
 module.exports = {
     cooldown: 5,
     data: new SlashCommandBuilder()
-    .setName('levels-hs')
-    .setDescription('Searches a user\'s skills on the hiscores')
+    .setName('clues-hs')
+    .setDescription('Searches a user\'s clue score on the hiscores')
     .addStringOption(option =>
         option.setName('username')
             .setDescription('The username to lookup.')
@@ -25,16 +25,16 @@ module.exports = {
         .setTitle(hiscoreData.username)
         // .setURL(itemData.wiki_url)
         // .setAuthor({ name: 'From oldschooljs', iconURL: 'https://i.imgur.com/KpaqfmQ.png', url: 'https://github.com/oldschoolgg/oldschooljs' })
-        .setDescription(`Hiscores for ${hiscoreData.username}, currently ${hiscoreData.skills.overall.rank} rank overall!`)
+        .setDescription(`Hiscores for ${hiscoreData.username}`)
         .setThumbnail('https://oldschool.runescape.wiki/images/Old_School_RuneScape_client_icon_%28Windows%29.png?83ac0')
         .setTimestamp();
-        for (const [skill, values] of Object.entries(hiscoreData.skills)) {
+        for (const [clue, values] of Object.entries(hiscoreData.clues)) {
             if (values.rank <= 0) {
                 continue;
             };
             userEmbed.addFields(
                 // { name: skill, value: `level: ${values.level.toString()}, xp: ${values.xp.toString()}, rank: ${values.rank.toString()}`, inline: true },
-                { name: skill, value: `${bold('level')}: ${values.level.toString()}, ${bold('xp')}: ${values.xp.toString()}, ${bold('rank')}: ${values.rank.toString()}`, inline: true },
+                { name: clue, value: `${bold('score')}: ${values.score.toString()} ${bold('rank')}: ${values.rank.toString()}`, inline: true },
             );
         }
         await interaction.editReply({ embeds: [userEmbed] });
